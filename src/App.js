cb2f31a62f4935b0fffc6e07f974f91d1021f8df
Matteo Fortunelli <125595035+fortunellimatteo@users.noreply.github.com>
2026-07-03 09:41:27 +0000

    change version

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          PROVAAAAAAAAAA
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 2
       </p>
     </div>
   );
